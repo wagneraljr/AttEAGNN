c62d2966ebd23463ee43d1e237c3d2e83db23d3d
Wagner Almeida <69909128+wagneraljr@users.noreply.github.com>
2024-07-31 16:14:54 -0300

    Update models.py

--- models.py
+++ models.py
@@ -94,7 +94,6 @@ class COMEdgeGraph(torch.nn.Module):
     def forward(self, agg_feat):
         return self.mlp(agg_feat)
 
-# Process node graph features for the CAGNN model
 class AGGNodeGraph(torch.nn.Module):
     def __init__(self, in_node_feats, hidden_size):
         super(AGGNodeGraph, self).__init__()
@@ -120,7 +119,6 @@ class COMNodeGraph(torch.nn.Module):
     def forward(self, agg_feat):
         return self.mlp(agg_feat)
 
-# Defines a single CAGNN model layer
 class CAGNNLayer(torch.nn.Module):
     def __init__(self, in_node_feats, in_edge_feats, hidden_size):
         super(CAGNNLayer, self).__init__()
@@ -143,21 +141,19 @@ class CAGNNLayer(torch.nn.Module):
 
         return new_node_feats, new_edge_feats
 
-# CAGNN model implementation
 class CAGNN(torch.nn.Module):
-    def __init__(self, num_layers, in_node_feats, in_edge_feats, hidden_size, out_feats, dropout):
+    def __init__(self, num_layers, in_node_feats, in_edge_feats, hidden_size, out_feats):
         super(CAGNN, self).__init__()
         self.layers = torch.nn.ModuleList()
         self.layers.append(CAGNNLayer(in_node_feats, in_edge_feats, hidden_size))
         for _ in range(num_layers - 1):
             self.layers.append(CAGNNLayer(hidden_size, hidden_size, hidden_size))
         self.final_layer = torch.nn.Linear(hidden_size, out_feats)
-        self.dropout = dropout
 
     def forward(self, node_neighbors, edge_neighbors, node_feats, edge_feats):
         for layer in self.layers:
             node_feats, edge_feats = layer(node_neighbors, edge_neighbors, node_feats, edge_feats)
-        node_feats = F.dropout(node_feats, self.dropout, training=self.training)
+        # Representação final dos nós
         out = self.final_layer(node_feats)
         return out
    
