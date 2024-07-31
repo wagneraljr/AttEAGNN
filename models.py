a38345783eec3d51bbafffd65796370087467c97
Wagner Almeida <69909128+wagneraljr@users.noreply.github.com>
2024-07-31 13:25:25 -0300

    Update models.py

--- models.py
+++ models.py
@@ -145,18 +145,19 @@ class CAGNNLayer(torch.nn.Module):
 
 # CAGNN model implementation
 class CAGNN(torch.nn.Module):
-    def __init__(self, num_layers, in_node_feats, in_edge_feats, hidden_size, out_feats):
+    def __init__(self, num_layers, in_node_feats, in_edge_feats, hidden_size, out_feats, dropout):
         super(CAGNN, self).__init__()
         self.layers = torch.nn.ModuleList()
         self.layers.append(CAGNNLayer(in_node_feats, in_edge_feats, hidden_size))
         for _ in range(num_layers - 1):
             self.layers.append(CAGNNLayer(hidden_size, hidden_size, hidden_size))
         self.final_layer = torch.nn.Linear(hidden_size, out_feats)
+        self.dropout = dropout
 
     def forward(self, node_neighbors, edge_neighbors, node_feats, edge_feats):
         for layer in self.layers:
             node_feats, edge_feats = layer(node_neighbors, edge_neighbors, node_feats, edge_feats)
-
+        node_feats = F.dropout(node_feats, self.dropout, training=self.training)
         out = self.final_layer(node_feats)
         return out
    
